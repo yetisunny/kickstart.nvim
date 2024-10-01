a0f5d692250cb9dd0a4a1caebf1c4b0c961a37a6
Luuk van der Heijden <luukjamal1404@hotmail.com>
2024-10-02 00:00:34 +0200

    add substitute nvim binds

--- init.lua
+++ init.lua
@@ -729,6 +729,14 @@ require('lazy').setup({
       },
     },
   },
+  {
+    'gbprod/substitute.nvim',
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
 
   { -- Autocompletion
     'hrsh7th/nvim-cmp',
@@ -1181,4 +1189,9 @@ require('oil').setup {
   keymaps_help = {
     border = 'rounded',
   },
-}
+} -- Lua
+
+vim.keymap.set('n', 's', require('substitute').operator, { noremap = true })
+vim.keymap.set('n', 'ss', require('substitute').line, { noremap = true })
+vim.keymap.set('n', 'S', require('substitute').eol, { noremap = true })
+vim.keymap.set('x', 's', require('substitute').visual, { noremap = true })
