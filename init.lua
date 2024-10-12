25175132437aec250dd6bc5ee2a2b375d2261904
Luuk van der Heijden <luukjamal1404@hotmail.com>
2024-10-12 13:31:22 +0200

    add copilot

--- init.lua
+++ init.lua
@@ -203,7 +203,7 @@ require('lazy').setup({
       },
     },
   },
-
+  { 'github/copilot.vim' },
   {
     'sethen/line-number-change-mode.nvim',
     config = function()
@@ -852,24 +852,6 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
-  },
-
   { 'norcalli/nvim-colorizer.lua' },
   { 'nvchad/volt', lazy = true },
   { 'nvchad/minty', lazy = true },
@@ -1008,6 +990,7 @@ require('treesitter-context').setup {
   zindex = 20, -- The Z-index of the context window
   on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
 }
+
 require('oil').setup {
   -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
   -- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
