008eaab06cd1a3e665d18bbad368adcff3906e66
Luuk van der Heijden <luukjamal1404@hotmail.com>
2024-10-12 13:35:12 +0200

    add some basic autosaving

--- init.lua
+++ init.lua
@@ -152,6 +152,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd({ 'BufLeave', 'FocusLost' }, {
+  callback = function()
+    if vim.bo.modified and not vim.bo.readonly and vim.fn.expand '%' ~= '' and vim.bo.buftype == '' then
+      vim.api.nvim_command 'silent update'
+    end
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
