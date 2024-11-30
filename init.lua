896900f2dbd81e957e68c42aada75354c0f17405
Luuk van der Heijden <luukjamal1404@hotmail.com>
2024-11-30 20:20:33 +0100

    add neotree and remove treesitter textobjectts, cus I didn't use it

--- init.lua
+++ init.lua
@@ -409,6 +409,16 @@ require('lazy').setup({
   -- },
 
   { 'nvim-treesitter/nvim-treesitter-context' },
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+  },
   -- {
   --   'mawkler/modicator.nvim',
   --   dependencies = 'mawkler/onedark.nvim', -- Add your colorscheme plugin here
@@ -1627,50 +1637,6 @@ end
 -- vim.api.nvim_set_hl(0, 'Search', { bg = '#cba6f7', fg = '#ffffff' })
 -- vim.api.nvim_set_hl(0, 'IncSearch', { bg = '#cba6f7', fg = '#ffffff' })
 
-require('nvim-treesitter.configs').setup {
-  textobjects = {
-    select = {
-      enable = true,
-
-      -- Automatically jump forward to textobj, similar to targets.vim
-      lookahead = true,
-
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        -- You can optionally set descriptions to the mappings (used in the desc parameter of
-        -- nvim_buf_set_keymap) which plugins like which-key display
-        ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class region' },
-        -- You can also use captures from other query groups like `locals.scm`
-        ['as'] = { query = '@local.scope', query_group = 'locals', desc = 'Select language scope' },
-      },
-      -- You can choose the select mode (default is charwise 'v')
-      --
-      -- Can also be a function which gets passed a table with the keys
-      -- * query_string: eg '@function.inner'
-      -- * method: eg 'v' or 'o'
-      -- and should return the mode ('v', 'V', or '<c-v>') or a table
-      -- mapping query_strings to modes.
-      selection_modes = {
-        ['@parameter.outer'] = 'v', -- charwise
-        ['@function.outer'] = 'V', -- linewise
-        ['@class.outer'] = '<c-v>', -- blockwise
-      },
-      -- If you set this to `true` (default is `false`) then any textobject is
-      -- extended to include preceding or succeeding whitespace. Succeeding
-      -- whitespace has priority in order to act similarly to eg the built-in
-      -- `ap`.
-      --
-      -- Can also be a function which gets passed a table with the keys
-      -- * query_string: eg '@function.inner'
-      -- * selection_mode: eg 'v'
-      -- and should return true or false
-      include_surrounding_whitespace = true,
-    },
-  },
-}
 require('spider').setup {
   skipInsignificantPunctuation = false,
   consistentOperatorPending = false, -- see "Consistent Operator-pending Mode" in the README
@@ -1681,3 +1647,5 @@ require('spider').setup {
 vim.keymap.set({ 'n', 'o', 'x' }, 'w', "<cmd>lua require('spider').motion('w')<CR>", { desc = 'Spider-w' })
 vim.keymap.set({ 'n', 'o', 'x' }, 'e', "<cmd>lua require('spider').motion('e')<CR>", { desc = 'Spider-e' })
 vim.keymap.set({ 'n', 'o', 'x' }, 'b', "<cmd>lua require('spider').motion('b')<CR>", { desc = 'Spider-b' })
+
+vim.keymap.set({ 'n', 'o', 'x' }, 'W', "<cmd>lua require('spider').motion('W')<CR>", { desc = 'Spider-W' })
