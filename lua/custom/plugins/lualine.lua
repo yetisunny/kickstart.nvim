4d9f28dc8726f972b4363287bd681ea3378043bf
Luuk van der Heijden <luukjamal1404@hotmail.com>
2025-01-31 22:15:12 +0100

    add diagnostics to lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -10,7 +10,7 @@ return {
       },
       sections = {
         lualine_a = { 'mode' },
-        lualine_b = { 'branch' },
+        lualine_b = { 'branch', 'diagnostics' },
         lualine_c = { 'filename' },
         lualine_x = { 'encoding', 'fileformat', 'filetype' },
         lualine_y = { 'progress' },
