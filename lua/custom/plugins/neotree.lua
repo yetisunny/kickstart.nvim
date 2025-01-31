f20bbd4bdca91572764aeec7e09c727d9aea8f45
Luuk van der Heijden <luukjamal1404@hotmail.com>
2025-01-31 22:15:18 +0100

    add a way to open neotree on the right

--- lua/custom/plugins/neotree.lua
+++ lua/custom/plugins/neotree.lua
@@ -7,4 +7,7 @@ return {
     'MunifTanjim/nui.nvim',
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
+  config = function()
+    vim.api.nvim_set_keymap('n', '<leader>tr', ':Neotree toggle right reveal<CR>', { noremap = true, silent = true })
+  end,
 }
