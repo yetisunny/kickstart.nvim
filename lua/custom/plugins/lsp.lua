6572080e84538c21b6ddd05f0799b2c7665bc8e5
Luuk van der Heijden <luukjamal1404@hotmail.com>
2025-04-11 21:45:40 +0200

    java changes

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -15,7 +15,7 @@ return {
     'hrsh7th/cmp-nvim-lsp',
   },
   config = function()
-    -- require('java').setup()
+    require('java').setup()
     -- Brief aside: **What is LSP?**
     --
     -- LSP is an initialism you've probably heard, but might not understand what it is.
@@ -220,6 +220,6 @@ return {
         end,
       },
     }
-    require('lspconfig').jdtls.setup {}
+    -- require('lspconfig').jdtls.setup {}
   end,
 }
