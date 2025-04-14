0b7bc3cda111d687447e5da128e1787f06da1c17
Luuk van der Heijden <luukjamal1404@hotmail.com>
2025-04-14 17:02:08 +0200

    only show current working files

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -62,7 +62,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
             height = 0.9,
             preview_height = 0.5,
           },
-        }
+        },
       },
       extensions = {
         ['ui-select'] = {
@@ -85,7 +85,9 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+    vim.keymap.set('n', '<leader>s.', function()
+      builtin.oldfiles { cwd_only = true }
+    end, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
     -- Slightly advanced example of overriding default behavior and theme
